6dbdacb802b71b4ae5f25ddd824a3f09e21783ad
SebaChuaqui <schuaquicano@gmail.com>
2022-06-02 12:36:14 -0300

    Animales Salvajes I

---
+++ assets/js/clases/Animal.js
@@ -0,0 +1,46 @@
+export class Animal {
+    constructor(nombre, edad, img, comentarios, sonido) {
+        let Nombre = nombre;
+        let Edad = edad;
+        let Img = img;
+        let Comentarios = comentarios;
+        let Sonido = sonido;
+
+        this.getNombre = () => Nombre;
+        this.getEdad = () => Edad;
+        this.getImg = () => Img;
+        this.getComentarios = () => Comentarios;
+        this.getSonido = () => {
+            return Sonido;
+        }
+
+        this.setComentarios = (comentarios) => Comentarios = comentarios;
+
+    }
+
+    get Nombre() {
+        return this.getNombre();
+    }
+
+    get Edad(){
+        return this.getEdad();
+    }
+
+    get Img(){
+        return this.getImg();
+    }
+
+    get Comentarios(){
+        return this.getComentarios();
+    }
+
+    get Sonido(){
+        return this.getSonido();
+    }
+
+    set Comentarios(comentarios){
+        return this.setComentarios(comentarios);
+    }
+}
+
+console.log(new Animal('Oso', 33, '...', 'Maullar', 'Es tímido'));
